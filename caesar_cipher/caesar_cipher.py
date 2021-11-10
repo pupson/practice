c5d1ce7fc5f9e326560742969456ebec0c431d4f
pupson <pupson13@gmail.com>
2021-11-10 03:30:09 -0800

    Update caesar_cipher.py

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,37 +1,42 @@
 import caesar_art
 
-print(caesar_art.logo)
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def caesar_cipher(text, shift, direction):
+def caesar_cipher():
     
-    cipher = []
+    done = False
 
-    for letter in text:
-        if letter not in alphabet:
-            cipher += letter
-        else:
-            let_ind = alphabet.index(letter)
+    while done != True:
 
-            if direction == 'encode':
-                let_shift = let_ind + shift
-                if let_shift > 26:
-                    let_shift -= 26
+        print(caesar_art.logo)
+
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        
+        cipher = []
+        shift = shift % 26
+        
+        for letter in text:
+            if letter not in alphabet:
+                cipher += letter
+            else:
+                let_ind = alphabet.index(letter)
+
+                if direction == 'encode':
+                    let_shift = let_ind + shift
+
+                else:                
+                    let_shift = let_ind - shift
+                
                 cipher += alphabet[let_shift]
 
-            else:                
-                let_shift = let_ind - shift
-                if let_shift < 0:
-                    let_shift += 26
-                cipher += alphabet[let_shift]
-
-    secret = ''.join(cipher)
-    print(f"The {direction}d text is {secret}")
+        secret = ''.join(cipher)
+        print(f"The {direction}d text is {secret}")
+        
+        repeat = input(f"Do you want to try again? Yes/No: ").lower()
+        if repeat == "no":
+            done = True
 
 
-caesar_cipher(text, shift, direction)
+caesar_cipher()
